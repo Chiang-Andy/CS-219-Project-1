f54fdc30c1b541e3556c6efa8d63ff966ba87b1e
Andy Chiang <andychiang@Andys-MacBook-Air.local>
2022-01-28 22:14:35 -0800

    final solution

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,26 +1,23 @@
-
 #include "analyzer.h"
 
-//getHex
-//getting hex values from file
+//Get the Hex object
+//stringstream hex from hex.txt
 uint32_t getHex(uint32_t hex){
-    stringstream ss;
-    ss << hex;
-    ss >> hex;
+    stringstream s;
+    s << hex;
+    s >> hex;
     return hex;
 } 
 
-//hexConversion
-//converting of hex decimal
-//fills 0 for hex with 0x format
+//conversion of hex decimal
 void hexConversion(uint32_t decimal){
-    cout << "0x" << setfill('0') << setw(8) << hex << decimal << endl;
+    cout << "0x" << hex << decimal << endl; //"0x" to set format for final value
 } 
 
 //hexAddition
 //adding decimal values
 uint32_t hexAddition(uint32_t d1, uint32_t d2){
-    uint32_t answer;
+    uint32_t answer; //uint32_t for answer to prevent issues
     answer = d1 + d2;
     return answer;
 }
--- driver.cpp
+++ driver.cpp
@@ -1,29 +1,26 @@
-
 #include "analyzer.h"
 
 //main
 int main(){
     string FILENAME;
     cout << "Input the filename." << endl;
+    cout << "Example: filename.txt" << endl;
     cin >> FILENAME;
-    uint32_t decimal;
 
     ifstream file(FILENAME);
     if(file.is_open()){
         string add;
         uint32_t a, b;
-        while(file >> add >> hex >> a >> b){
-            cout << add << " ";
-            cout << "0x" << a << " + ";
+        while(file >> add >> hex >> a >> b){ 
+            cout << "0x" << a << " + "; 
             cout << "0x" << b << " = ";
+
             uint32_t d1 = getHex(a);
             uint32_t d2 = getHex(b);
             hexConversion(hexAddition(d1, d2));
         }
     }
     else{
-        cout << "Please input correct filename" << endl;
-        cout << "Example: filename.txt" << endl;
+        cout << "Please input correct filename." << endl;
     }
-    file.close();
 }
