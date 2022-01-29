3c3a4634bcdcd65a656fd1db8e484dc1fe825333
Andy Chiang <andychiang@Andys-MacBook-Air.local>
2022-01-28 23:06:44 -0800

    final solution

--- analyzer.cpp
+++ analyzer.cpp
@@ -9,11 +9,6 @@ uint32_t getHex(uint32_t hex){
     return hex;
 } 
 
-//conversion of hex decimal
-void hexConversion(uint32_t decimal){
-    cout << "0x" << hex << decimal << endl; //"0x" to set format for final value
-} 
-
 //hexAddition
 //adding decimal values
 uint32_t hexAddition(uint32_t d1, uint32_t d2){
@@ -21,3 +16,8 @@ uint32_t hexAddition(uint32_t d1, uint32_t d2){
     answer = d1 + d2;
     return answer;
 }
+
+//conversion of hex decimal
+void hexConversion(uint32_t decimal){
+    cout << "0x" << hex << decimal << endl; //"0x" to set format for final value
+} 
