b454424d8e2fabb6936fca1bebe016c5c36e550a
Chiang-Andy <77953300+Chiang-Andy@users.noreply.github.com>
2022-01-28 21:07:37 -0800

    Add files via upload

---
+++ CS 219 Project 1/analyzer.cpp
@@ -0,0 +1,25 @@
+#include "analyzer.h"
+
+//getHex
+//getting hex values from file
+uint32_t getHex(uint32_t hex){
+    stringstream ss;
+    ss << hex;
+    ss >> hex;
+    return hex;
+} 
+
+//hexConversion
+//converting of hex decimal
+//fills 0 for hex with 0x format
+void hexConversion(uint32_t decimal){
+    cout << "0x" << setfill('0') << setw(8) << hex << decimal << endl;
+} 
+
+//hexAddition
+//adding decimal values
+uint32_t hexAddition(uint32_t d1, uint32_t d2){
+    uint32_t answer;
+    answer = d1 + d2;
+    return answer;
+}
---
+++ CS 219 Project 1/analyzer.h
@@ -0,0 +1,15 @@
+#ifndef ANALYZER_H
+#define ANALYZER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+uint32_t getHex(uint32_t);
+void hexConversion(uint32_t);
+uint32_t hexAddition(uint32_t, uint32_t);
+
+#endif
